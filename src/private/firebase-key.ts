44614bea5a8fd9f6763300e835dcb9551531049b
chaliya96 <chalith1@gmail.com>
2018-11-25 19:23:06 +0530

    Initial commit

---
+++ src/private/firebase-key.ts
@@ -0,0 +1,9 @@
+export const fireBaseKey = {
+    apiKey: "AIzaSyBN9eUu07p3ie9JyQL1UQsyi8aEEIw71vs",
+    authDomain: "spicy-489ea.firebaseapp.com",
+    databaseURL: "https://spicy-489ea.firebaseio.com",
+    projectId: "spicy-489ea",
+    storageBucket: "spicy-489ea.appspot.com",
+    messagingSenderId: "29976015064"
+  };
+
